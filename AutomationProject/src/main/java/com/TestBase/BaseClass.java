5b490939b9e9e561a16040d8e105bc18d64328fa
Yogeshwaran1018 <yogeshwaran1018@gmail.com>
2024-09-02 11:53:26 +0530

    Added getdriver method with chrome

--- AutomationProject/src/main/java/com/TestBase/BaseClass.java
+++ AutomationProject/src/main/java/com/TestBase/BaseClass.java
@@ -1,5 +1,19 @@
 package com.TestBase;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BaseClass {
 
+	
+	public static WebDriver driver;
+	
+	public WebDriver getDriver(String browserName)
+	{
+		if(browserName.equalsIgnoreCase("chrome"))
+		{
+			driver=new ChromeDriver();
+		}
+		return driver;
+	}
 }
